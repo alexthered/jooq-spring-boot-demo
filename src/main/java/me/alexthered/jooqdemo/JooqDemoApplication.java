c2daf1ca4eaa4cc2ceeca1857c23ddb49336ddc3
HIep Doan <hiep.dv179@gmail.com>
2020-05-12 23:19:59 +0200

    🎉 Initialize the project

---
+++ src/main/java/me/alexthered/jooqdemo/JooqDemoApplication.java
@@ -0,0 +1,13 @@
+package me.alexthered.jooqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JooqDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/alexthered/jooqdemo/JooqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alexthered.jooqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
